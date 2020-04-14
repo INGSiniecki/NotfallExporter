27c4ea3551add7ed00aa79308aa622a5bf297e68
Kevin Siniecki <kevinsiniecki@hotmail.de>
2020-04-14 11:27:24 +0200

    Log-Fehler beheben

--- NotfallExporterLib/Util/Log.cs
+++ NotfallExporterLib/Util/Log.cs
@@ -7,6 +7,6 @@ namespace Com.Ing.DiBa.NotfallExporterLib.Util
     /// </summary>
     public static class Log
     {
-         static log4net.ILog Logger { get; } = log4net.LogManager.GetLogger(System.Reflection.MethodBase.GetCurrentMethod().DeclaringType);
+         public static log4net.ILog Logger { get; } = log4net.LogManager.GetLogger(System.Reflection.MethodBase.GetCurrentMethod().DeclaringType);
     }
 }
