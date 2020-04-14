753a239e267f8669d747be379bc859afb1f10983
Kevin Siniecki <kevinsiniecki@hotmail.de>
2020-04-14 11:29:19 +0200

    kleine Anpassung

--- NotfallExporterUI/Program.cs
+++ NotfallExporterUI/Program.cs
@@ -1,21 +1,16 @@
 ﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Threading.Tasks;
 using System.Windows.Forms;
 [assembly: log4net.Config.XmlConfigurator(Watch = true)]
 namespace NotfallExporterUI
 {
     static class Program
     {
-        private static readonly log4net.ILog log = log4net.LogManager.GetLogger(System.Reflection.MethodBase.GetCurrentMethod().DeclaringType);
         /// <summary>
         /// Der Haupteinstiegspunkt für die Anwendung.
         /// </summary>
         [STAThread]
         static void Main()
         {
-            log.Fatal("hallo");
             Application.EnableVisualStyles();
             Application.SetCompatibleTextRenderingDefault(false);
             Application.Run(new Form1());
